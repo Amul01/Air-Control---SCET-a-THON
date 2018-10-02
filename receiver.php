33e9731bb9dd55e8ed49ab38cf61f3a4ae18db56
Amul Luniya <amul0128india@gmail.com>
2018-10-02 15:05:18 +0530

    Add files via upload

--- receiver.php
+++ receiver.php
@@ -1,13 +1,50 @@
+<!DOCTYPE html
+<html>
+<head>
+<h1>Welcome to air control</h1>
+</head>
+<body style="background-color:yellow">
+</body>
+</html>
 <?php
-	$username = "root";  // enter database username, I used "arduino" in step 2.2
-    $password = "password";  // enter database password, I used "arduinotest" in step 2.2
-    $servername = "localhost"; // IMPORTANT: if you are using XAMPP enter "localhost"
-	$db = "presensi";
-    // Connect to your database
-    $link = mysqli_connect($servername, $username, $password, $db);
-	$sql = "SELECT * FROM test.transmitflag";
-	$query = mysqli_query($link, $sql);
-	while($row = mysqli_fetch_array($query)){
-		echo $row['status'].',';
+	$dbusername = "root";
+	$dbpassword = "";
+	$server = "localhost";
+	$dbname = "abc";
+	
+	echo "";
+	echo "Starting connection\r\n";
+	$dbconnect = mysqli_connect($server, $dbusername, $dbpassword, $dbname);
+	echo "\r\n";
+	echo "You are connected\r\n";
+	
+	$sql = "select * from abc.gas order by id DESC LIMIT 10";
+	$query = mysqli_query($dbconnect, $sql);
+	
+	/*while($row = mysqli_fetch_array($query)){
+		echo "\n";
+		echo nl2br("\r\n".$row['co2']."    ".$row['co']."    ".$row['nh4']);
+	}*/
+	
+	echo 
+	"<table border=1>
+	<tr>
+	<th>ID</th>
+	<th>co2</th>
+	<th>co</th>
+	<th>nh4</th>
+	</tr>";
+
+	while($row = mysqli_fetch_array($query))
+	{
+		echo "<tr>";
+		echo "<td>" . $row['ID'] . "</td>";
+		echo "<td>" . $row['co2'] . "</td>";
+		echo "<td>" . $row['co'] . "</td>";
+		echo "<td>" . $row['nh4'] . "</td>";
+		echo "</tr>";
 	}
+	echo "</table>";
+		
+	mysqli_close($dbconnect);
 ?>
